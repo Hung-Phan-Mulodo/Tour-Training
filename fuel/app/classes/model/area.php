21a75848bf4de9743baebb03ad5042e876027cbb
phan-hung-mulodo <phan.hung@mulodo.com>
2018-03-06 13:33:08 +0700

    reformat code style with PSR2

--- fuel/app/classes/model/area.php
+++ fuel/app/classes/model/area.php
@@ -2,29 +2,31 @@
 
 class Model_Area extends \Orm\Model
 {
-	protected static $_properties = array(
-		'id',
-		'code',
-		'name',
-		'country',
-		'country_name',
-		'city',
-		'city_name',
-		'created_at',
-		'updated_at',
-	);
+    protected static $_properties
+        = array(
+            'id',
+            'code',
+            'name',
+            'country',
+            'country_name',
+            'city',
+            'city_name',
+            'created_at',
+            'updated_at',
+        );
 
-	protected static $_observers = array(
-		'Orm\Observer_CreatedAt' => array(
-			'events' => array('before_insert'),
-			'mysql_timestamp' => false,
-		),
-		'Orm\Observer_UpdatedAt' => array(
-			'events' => array('before_update'),
-			'mysql_timestamp' => false,
-		),
-	);
+    protected static $_observers
+        = array(
+            'Orm\Observer_CreatedAt' => array(
+                'events' => array('before_insert'),
+                'mysql_timestamp' => false,
+            ),
+            'Orm\Observer_UpdatedAt' => array(
+                'events' => array('before_update'),
+                'mysql_timestamp' => false,
+            ),
+        );
 
-	protected static $_table_name = 'areas';
+    protected static $_table_name = 'areas';
 
 }
