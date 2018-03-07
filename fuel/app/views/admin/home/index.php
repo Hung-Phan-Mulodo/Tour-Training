2f5050c97c8bc519da1239274359d5584b88f0ce
phan-hung-mulodo <phan.hung@mulodo.com>
2018-03-07 16:13:48 +0700

    Build admin themeplate

---
+++ fuel/app/views/admin/home/index.php
@@ -0,0 +1,21 @@
+<div class="page-content-wrapper">
+    <!-- BEGIN CONTENT BODY -->
+    <div class="page-content">
+        <div class="page-bar x_panel">
+            <ul class="page-breadcrumb">
+                <li>
+                    <a href="index.html">Home</a>
+                    <i class="fa fa-circle"></i>
+                </li>
+                <li>
+                    <span>Dashboard</span>
+                </li>
+            </ul>
+        </div>
+        <!-- END PAGE BAR -->
+        <!-- BEGIN PAGE TITLE-->
+        <h1 class="page-title"> Welcome, <?php echo $name; ?>!
+        </h1>
+    </div>
+    <!-- END CONTENT BODY -->
+</div>
